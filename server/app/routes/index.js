d00116b3e833fd7e68bd02d2747a99b530d87e89
Sarah Edkins <edkins.sarah@gmail.com>
2015-11-02 16:39:58 -0500

    review schema and routes

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -4,6 +4,7 @@ module.exports = router;
 
 router.use('/members', require('./members'));
 router.use('/carts', require('./carts'));
+router.use('/reviews', require('./reviews'));
 
 // Make sure this is after all of
 // the registered routes!
---
+++ server/app/routes/reviews/index.js
@@ -0,0 +1,48 @@
+'use strict';
+var router = require('express').Router();
+module.exports = router;
+var _ = require('lodash');
+
+var Review = require('../../../db/models/review');
+
+router.get('/', function(req, res, next){
+    Review.find()
+        .then(function(reviews){
+            res.send(reviews);
+        })
+})
+
+router.get('/product/:productId', function(req, res, next){
+    Review.find({ product_id: req.params.productId })
+        .then(function(reviews){
+            res.send(reviews);
+        })
+})
+
+router.get('/user/:userId', function(req, res, next){
+    Review.find({ user_id: req.params.userId })
+        .then(function(reviews){
+            res.send(reviews);
+        })
+})
+
+router.post('/', function(req, res, next){
+    Review.create(req.body)
+        .then(function(review){
+            res.send(review);
+        })
+})
+
+router.put('/:id', function(req, res, next){
+    Review.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        .then(function(review){
+            res.send(review);
+        })
+})
+
+router.delete('/:id', function(req, res, next){
+    Review.findByIdAndRemove(req.params.id)
+        .then(function(review){
+            res.send(review);
+        })
+})
