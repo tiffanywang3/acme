8302fa9aeaaa2b44e18ae8c2ba4c1b08f98f52a7
Sarah Edkins <edkins.sarah@gmail.com>
2015-11-02 16:24:14 -0500

    cart schema and routes

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -3,6 +3,7 @@ var router = require('express').Router();
 module.exports = router;
 
 router.use('/members', require('./members'));
+router.use('/carts', require('./carts'));
 
 // Make sure this is after all of
 // the registered routes!
