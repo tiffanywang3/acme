1fac2bac7b2f4e49205cff32f161092525f17c0d
Tiffany W <tiffany.wang777@gmail.com>
2015-11-02 16:02:22 -0500

    update user routes

---
+++ server/app/routes/users/index.js
@@ -0,0 +1,48 @@
+'use strict';
+var router = require('express').Router();
+module.exports = router;
+var _ = require('lodash');
+
+
+router.get('/:user_id', function(req, res, next){
+	User.findById(req.params.user_id)
+	.then(function(user){
+		//req.requestedUser = user ????
+		res.send(user);
+	}, function(err){
+		res.status(404).next();
+	})
+	.then(null, next);
+})
+
+router.put('/:user_id', function(req, res, next){
+	User.findByIdAndUpdate(req.params.user_id, req.body)
+	.then(function(user){
+		res.send(user);
+	}, function(err){
+		res.status(404).next();
+	})
+	.then(null, next);
+})
+
+router.delete('/:user_id', function(req, res, next){
+	User.findByIdAndRemove(req.params.user_id)
+	.then(function(){
+		res.status(204).end();
+	}, function(err){
+		res.status(404).end();
+	})
+	.then(null, next);
+})
+
+
+
+
+
+
+
+
+
+
+
+
