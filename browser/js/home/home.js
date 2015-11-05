fbae17514012d2d8dc52e84654c865b3c13842d5
Philip <png262@gmail.com>
2015-11-05 14:23:19 -0500

    started front end main page

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -1,6 +1,18 @@
 app.config(function ($stateProvider) {
     $stateProvider.state('home', {
         url: '/',
-        templateUrl: 'js/home/home.html'
+        templateUrl: 'js/home/home.html',
+        controller: 'HomeCtrl'
     });
+});
+
+app.controller('HomeCtrl', function ($scope, AuthService, $state) {
+
+    $scope.login = {};
+    $scope.error = null;
+    //$scope.allProducts = allProducts;
+
+    $scope.allProducts = [{product_name:"test produ",unitPrice:"333"},{product_name:"test product2",unitPrice:"444"} ]
+    
+
 });
