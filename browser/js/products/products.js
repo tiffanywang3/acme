9a03c8df05ac3470dc674ac1ef8047dc974456c2
Philip <png262@gmail.com>
2015-11-05 16:25:20 -0500

    completed productTile directive

---
+++ browser/js/products/products.js
@@ -0,0 +1,23 @@
+app.config(function ($stateProvider) {
+    $stateProvider.state('products', {
+        url: '/',
+        templateUrl: 'js/products/products.html',
+        controller: 'ProductsCtrl',
+        resolve: {
+        	allProducts: function(ProductFactory) {
+        		return ProductFactory.fetchAll();
+        	}
+        }
+    });
+});
+
+app.controller('ProductsCtrl', function (allProducts, $scope, AuthService, $state) {
+
+    $scope.login = {};
+    $scope.error = null;
+    $scope.allProducts = allProducts;
+
+    // $scope.allProducts = [{product_name:"test produ",unitPrice:"333",imageUrl:"/images/adventuretime_finnsword.jpg"},{product_name:"test product2",unitPrice:"444",imageUrl:"/images/adventuretime_gumbball.jpg"} ,{product_name:"test product3",unitPrice:"666",imageUrl:"/images/adventuretime_finnhat.jpg"}, {product_name:"test product4",unitPrice:"666",imageUrl:"/images/adventuretime_finnhat.jpg"}, {product_name:"test product5",unitPrice:"666",imageUrl:"/images/adventuretime_finnhat.jpg"}, {product_name:"test product6",unitPrice:"666", imageUrl:"/images/placeholder.png"}]
+    
+
+});
