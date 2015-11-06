a6b465e55e8b1602d75fc8aa8bc652bab33dc923
Sarah Edkins <edkins.sarah@gmail.com>
2015-11-06 16:09:40 -0500

    directive pretty done

--- server/app/routes/reviews/index.js
+++ server/app/routes/reviews/index.js
@@ -9,41 +9,42 @@ router.get('/', function(req, res, next){
     Review.find()
         .then(function(reviews){
             res.send(reviews);
-        }).then(null, next)
+        })
 })
 
-// possibly switch to /products/:id/reviews
 router.get('/product/:productId', function(req, res, next){
     Review.find({ product_id: req.params.productId })
+        .populate("user_id")
+        .exec()
         .then(function(reviews){
             res.send(reviews);
-        }).then(null, next)
+        })
 })
 
 router.get('/user/:userId', function(req, res, next){
     Review.find({ user_id: req.params.userId })
         .then(function(reviews){
             res.send(reviews);
-        }).then(null, next)
+        })
 })
 
 router.post('/', function(req, res, next){
     Review.create(req.body)
         .then(function(review){
             res.status(201).send(review);
-        }).then(null, next)
+        })
 })
 
 router.put('/:id', function(req, res, next){
     Review.findByIdAndUpdate(req.params.id, req.body, { new: true })
         .then(function(review){
             res.send(review);
-        }).then(null, next)
+        })
 })
 
 router.delete('/:id', function(req, res, next){
     Review.findByIdAndRemove(req.params.id)
         .then(function(review){
             res.status(204).send(review);
-        }).then(null, next)
+        })
 })
--- server/db/models/review.js
+++ server/db/models/review.js
@@ -4,7 +4,6 @@ var mongoose = require("mongoose");
 var schema = new mongoose.Schema({
    user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
    product_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true},
-   title: { type: String, maxlength: 10 },
    text: { type: String, minlength: 10, maxlength: 250 },
    rating: { type: Number, min: 1, max: 5, required: true },
    date: { type: Date, default: Date.now }
