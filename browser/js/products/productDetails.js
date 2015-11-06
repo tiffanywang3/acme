5410808b667110a957b0872b78127aaea423e4a4
Uma Chandran <umachandran6@gmail.com>
2015-11-06 15:17:23 -0500

    adding product work so far

--- browser/js/products/productDetails.js
+++ browser/js/products/productDetails.js
@@ -20,11 +20,20 @@ app.controller('ProductDetailsCtrl', function (theProduct, $scope, AuthService, 
     // $scope.login = {};
     // $scope.error = null;
     $scope.product = theProduct;
-    $scope.reviews = reviews;
+   // $scope.reviews = reviews;
+    // $scope.productCategories = theProduct.categories;
     console.log("product details - the product", $scope.product)
-     console.log("product reviews - the reviews", $scope.reviews)
+     //console.log("product reviews - the reviews", $scope.reviews)
 
-    // $scope.allProducts = [{product_name:"test produ",unitPrice:"333",imageUrl:"/images/adventuretime_finnsword.jpg"},{product_name:"test product2",unitPrice:"444",imageUrl:"/images/adventuretime_gumbball.jpg"} ,{product_name:"test product3",unitPrice:"666",imageUrl:"/images/adventuretime_finnhat.jpg"}, {product_name:"test product4",unitPrice:"666",imageUrl:"/images/adventuretime_finnhat.jpg"}, {product_name:"test product5",unitPrice:"666",imageUrl:"/images/adventuretime_finnhat.jpg"}, {product_name:"test product6",unitPrice:"666", imageUrl:"/images/placeholder.png"}]
-    
+
+
+
+  $scope.getInventoryNum = function(inventory) {
+       if (inventory<20)
+        return new Array(inventory);
+       else
+        return new Array(20);
+   }
+
 
 });
