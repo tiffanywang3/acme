442f36e7f6b99ed7644bd486729c41315463544c
Sarah Edkins <edkins.sarah@gmail.com>
2015-11-08 20:58:28 -0500

    added error handlers back to review routes

--- server/app/routes/reviews/index.js
+++ server/app/routes/reviews/index.js
@@ -9,7 +9,7 @@ router.get('/', function(req, res, next){
     Review.find()
         .then(function(reviews){
             res.send(reviews);
-        })
+        }).then(null, next)
 })
 
 router.get('/product/:productId', function(req, res, next){
@@ -18,7 +18,7 @@ router.get('/product/:productId', function(req, res, next){
         .exec()
         .then(function(reviews){
             res.send(reviews);
-        })
+        }).then(null, next)
 })
 
 router.get('/user/:userId', function(req, res, next){
@@ -26,26 +26,26 @@ router.get('/user/:userId', function(req, res, next){
         .populate("product_id")
         .then(function(reviews){
             res.send(reviews);
-        })
+        }).then(null, next)
 })
 
 router.post('/', function(req, res, next){
     Review.create(req.body)
         .then(function(review){
             res.status(201).send(review);
-        })
+        }).then(null, next)
 })
 
 router.put('/:id', function(req, res, next){
     Review.findByIdAndUpdate(req.params.id, req.body, { new: true })
         .then(function(review){
             res.send(review);
-        })
+        }).then(null, next)
 })
 
 router.delete('/:id', function(req, res, next){
     Review.findByIdAndRemove(req.params.id)
         .then(function(review){
             res.status(204).send(review);
-        })
+        }).then(null, next)
 })
