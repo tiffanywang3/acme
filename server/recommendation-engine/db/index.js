6929c70b49d05b30423ee24cbf633ca7a61c4caa
Tiffany W <tiffany.wang777@gmail.com>
2015-11-11 12:58:56 -0500

    fix database

--- server/recommendation-engine/db/index.js
+++ server/recommendation-engine/db/index.js
@@ -2,11 +2,10 @@
 var Promise = require('bluebird');
 var path = require('path');
 var chalk = require('chalk');
-//var DATABASE_URI = require(path.join(__dirname, "mongodb://localhost:27017/fsg-app")).DATABASE_URI;
+var DATABASE_URI = require(path.join(__dirname, '../env')).DATABASE_URI;
 
 var mongoose = require('mongoose');
-var db = mongoose.connect("mongodb://localhost:27017/fsg-app").connection;
-
+var db = mongoose.connect(DATABASE_URI).connection;
 // Require our models -- these should register the model into mongoose
 // so the rest of the application can simply call mongoose.model('User')
 // anywhere the User model needs to be used.
